596b5a794c768b11c27025b211c4b5b3a56ee55e
Noyan.dev <ma13842791199@gmail.com>
2026-09-02 16:18:10 +0330

    react router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Chat from './pages/Chat'
 import Auth from './pages/Auth'
 import './App.css'
@@ -6,9 +6,13 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <Auth />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Auth />} />
+        <Route path="/chat" element={<Chat />}/>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
